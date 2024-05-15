744fa3a735d503ff71291f5d5a82bb8311fe8b8a
Andreas Böhm <60391995+matherm-aboehm@users.noreply.github.com>
2024-05-15 16:09:43 +0200

    chore: Change visibility of IReflectionOnlyModuleExtension to internal
    * dscom.client project needs also strong name signing to have
      InternalsVisibleToAttribute working
    * assembly reference from dscom.client to dscom must be aliased to avoid
      name conflicts, usings need to adjusted accordingly

--- src/dscom.client/TypeLibExporterNotifySink.cs
+++ src/dscom.client/TypeLibExporterNotifySink.cs
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+extern alias dscom;
+using dscom::dSPACE.Runtime.InteropServices;
 using System.Reflection;
 
 namespace dSPACE.Runtime.InteropServices;
