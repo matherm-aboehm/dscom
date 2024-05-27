23acee851c827b6d7d4cdfcf03e2b0818ffb8efc
Andreas Böhm <60391995+matherm-aboehm@users.noreply.github.com>
2024-05-27 14:36:11 +0200

    fix: Dependency on specific IDE or environment removed from unit tests
    * fix: Added thread synchronisation code to CLITest, to resolve race
      conditions when test directory will be cleaned up and tests assert for
      existing or missing files (at least under VSCode tests can be run on
      multiple threads and anti-virus software can worsen the problem)
    * fix: Tests should not assert on error messages that come from OS as it
      could be localized, use corresponding error code instead

--- src/dscom.test/tests/CLITest/CLITest.cs
+++ src/dscom.test/tests/CLITest/CLITest.cs
@@ -12,87 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-using System.Diagnostics;
-using System.Text;
-
 namespace dSPACE.Runtime.InteropServices.Tests;
-
-/// <summary>
-/// Provides the base implementation for running CLI tests.
-/// </summary>
-/// <remarks>The CLI tests are not available for .NET Framework</remarks>
-public abstract class CLITestBase : IClassFixture<CompileReleaseFixture>
-{
-    protected const string ErrorNoCommandOrOptions = "Required command was not provided.";
-
-    internal record struct ProcessOutput(string StdOut, string StdErr, int ExitCode);
-
-    internal string DSComPath { get; set; } = string.Empty;
-
-    internal string TestAssemblyPath { get; }
-
-    internal string TestAssemblyDependencyPath { get; }
-
-    public CLITestBase(CompileReleaseFixture compileFixture)
-    {
-        DSComPath = compileFixture.DSComPath;
-        TestAssemblyPath = compileFixture.TestAssemblyPath;
-        TestAssemblyDependencyPath = compileFixture.TestAssemblyDependencyPath;
-
-        RetryHandler.Retry(() =>
-        {
-            foreach (var file in Directory.EnumerateFiles(Environment.CurrentDirectory, "*.tlb"))
-            {
-                File.Delete(file);
-            };
-        }, new[] { typeof(UnauthorizedAccessException) });
-
-        RetryHandler.Retry(() =>
-        {
-            foreach (var file in Directory.EnumerateFiles(Environment.CurrentDirectory, "*.yaml"))
-            {
-                File.Delete(file);
-            };
-        }, new[] { typeof(UnauthorizedAccessException) });
-    }
-
-    internal static ProcessOutput Execute(string filename, params string[] args)
-    {
-        var processOutput = new ProcessOutput();
-        using var process = new Process();
-        process.StartInfo.UseShellExecute = false;
-        process.StartInfo.RedirectStandardOutput = true;
-        process.StartInfo.RedirectStandardError = true;
-        var sb = new StringBuilder();
-        process.ErrorDataReceived += new DataReceivedEventHandler((sender, e) => { sb.Append(e.Data); });
-        process.StartInfo.FileName = filename;
-        process.StartInfo.Arguments = string.Join(" ", args);
-        process.Start();
-
-        process.BeginErrorReadLine();
-        process.WaitForExit();
-        processOutput.StdOut = process.StandardOutput.ReadToEnd();
-        processOutput.StdErr = sb.ToString();
-        processOutput.ExitCode = process.ExitCode;
-
-        return processOutput;
-    }
-
-    /// <summary>
-    /// When running tests that involves the <c>tlbembed</c> command or <c>tlbexport</c> 
-    /// command with the <c>--embed</c> switch enabled, the test should call the function 
-    /// to handle the difference between .NET 4.8 which normally would expect COM objects 
-    /// to be defined in the generated assembly versus .NET 5+ where COM objects will be 
-    /// present in a <c>*.comhost.dll</c> instead.
-    /// </summary>
-    /// <param name="sourceAssemblyFile">The path to the source assembly from where the COM objects are defined.</param>
-    internal static string GetEmbeddedPath(string sourceAssemblyFile)
-    {
-        var embedFile = Path.Combine(Path.GetDirectoryName(sourceAssemblyFile) ?? string.Empty, Path.GetFileNameWithoutExtension(sourceAssemblyFile) + ".comhost" + Path.GetExtension(sourceAssemblyFile));
-        File.Exists(embedFile).Should().BeTrue($"File {embedFile} must exist prior to running the test.");
-        return embedFile;
-    }
-}
 
 /// <summary>
 /// The basic CLI tests to run.
@@ -209,6 +129,7 @@ public class CLITest : CLITestBase
     [StaFact]
     public void TlbExportAndDemoAssemblyAndCallWithTlbDump_ExitCodeIs0AndTlbIsAvailableAndValid()
     {
+        using var context = CompileFixture.GetPreparedTestDirectoryContext();
         var tlbFileName = $"{Path.GetFileNameWithoutExtension(TestAssemblyPath)}.tlb";
         var yamlFileName = $"{Path.GetFileNameWithoutExtension(TestAssemblyPath)}.yaml";
 
@@ -231,6 +152,7 @@ public class CLITest : CLITestBase
     [StaFact]
     public void TlbExportAndEmbedAssembly_ExitCodeIs0AndTlbIsEmbeddedAndValid()
     {
+        using var context = CompileFixture.GetPreparedTestDirectoryContext();
         var embedPath = GetEmbeddedPath(TestAssemblyPath);
 
         var tlbFileName = $"{Path.GetFileNameWithoutExtension(TestAssemblyPath)}.tlb";
@@ -256,6 +178,7 @@ public class CLITest : CLITestBase
     [StaFact]
     public void TlbExportCreateMissingDependentTLBsFalseAndOverrideTlbId_ExitCodeIs0AndTlbIsAvailableAndDependentTlbIsNot()
     {
+        using var context = CompileFixture.GetPreparedTestDirectoryContext();
         var tlbFileName = $"{Path.GetFileNameWithoutExtension(TestAssemblyPath)}.tlb";
         var tlbFilePath = Path.Combine(Environment.CurrentDirectory, tlbFileName);
 
@@ -274,6 +197,7 @@ public class CLITest : CLITestBase
     [StaFact]
     public void TlbExportCreateMissingDependentTLBsFalse_ExitCodeIs0AndTlbIsAvailableAndDependentTlbIsNot()
     {
+        using var context = CompileFixture.GetPreparedTestDirectoryContext();
         var tlbFileName = $"{Path.GetFileNameWithoutExtension(TestAssemblyPath)}.tlb";
         var dependentFileName = $"{Path.GetFileNameWithoutExtension(TestAssemblyDependencyPath)}.tlb";
         var tlbFilePath = Path.Combine(Environment.CurrentDirectory, tlbFileName);
@@ -292,6 +216,7 @@ public class CLITest : CLITestBase
     [StaFact]
     public void TlbExportCreateMissingDependentTLBsTrue_ExitCodeIs0AndTlbIsAvailableAndDependentTlbIsNot()
     {
+        using var context = CompileFixture.GetPreparedTestDirectoryContext();
         var tlbFileName = $"{Path.GetFileNameWithoutExtension(TestAssemblyPath)}.tlb";
         var dependentFileName = $"{Path.GetFileNameWithoutExtension(TestAssemblyDependencyPath)}.tlb";
         var tlbFilePath = Path.Combine(Environment.CurrentDirectory, tlbFileName);
@@ -307,6 +232,7 @@ public class CLITest : CLITestBase
     [StaFact]
     public void TlbExportCreateMissingDependentTLBsNoValue_ExitCodeIs0()
     {
+        using var context = CompileFixture.GetPreparedTestDirectoryContext();
         var tlbFileName = $"{Path.GetFileNameWithoutExtension(TestAssemblyPath)}.tlb";
         var dependentFileName = $"{Path.GetFileNameWithoutExtension(TestAssemblyDependencyPath)}.tlb";
         var tlbFilePath = Path.Combine(Environment.CurrentDirectory, tlbFileName);
@@ -322,6 +248,7 @@ public class CLITest : CLITestBase
     [StaFact]
     public void TlbExportAndOptionSilent_StdOutAndStdErrIsEmpty()
     {
+        using var context = CompileFixture.GetPreparedTestDirectoryContext();
         var tlbFileName = $"{Guid.NewGuid()}.tlb";
         var tlbFilePath = Path.Combine(Environment.CurrentDirectory, tlbFileName);
 
@@ -337,10 +264,13 @@ public class CLITest : CLITestBase
     [StaFact]
     public void TlbExportAndOptionSilenceTX801311A6andTX0013116F_StdOutAndStdErrIsEmpty()
     {
+        using var context = CompileFixture.GetPreparedTestDirectoryContext();
         var tlbFileName = $"{Guid.NewGuid()}.tlb";
         var tlbFilePath = Path.Combine(Environment.CurrentDirectory, tlbFileName);
 
-        var result = Execute(DSComPath, "tlbexport", TestAssemblyPath, "--out", tlbFileName, "--silence", "TX801311A6", "--silence", "TX0013116F");
+        // HACK: new test assemblies needs also to silence TX8013117D, TX800288C6,
+        // as long as they are referencing WPF libs
+        var result = Execute(DSComPath, "tlbexport", TestAssemblyPath, "--out", tlbFileName, "--silence", "TX801311A6", "--silence", "TX0013116F", "--silence", "TX8013117D", "--silence", "TX800288C6");
         result.ExitCode.Should().Be(0);
 
         File.Exists(tlbFilePath).Should().BeTrue($"File {tlbFilePath} should be available.");
@@ -352,6 +282,7 @@ public class CLITest : CLITestBase
     [StaFact]
     public void TlbExportAndOptionOverrideTLBId_TLBIdIsCorrect()
     {
+        using var context = CompileFixture.GetPreparedTestDirectoryContext();
         var guid = Guid.NewGuid().ToString();
 
         var tlbFileName = $"{guid}.tlb";
@@ -372,118 +303,5 @@ public class CLITest : CLITestBase
 
         var yamlContent = File.ReadAllText(yamlFilePath);
         yamlContent.Should().Contain($"guid: {guid}");
-    }
-}
-
-/// <summary>
-/// The tests for embeds are performed as a separate class due to the additional setup
-/// required and the need for creating a TLB file as part of export prior to testing
-/// the embed functionality itself. There are parallelization issues with running them,
-/// even within the same class. Thus, the separate class has additional setup during the
-/// constructor to perform the export once and ensure the process relating to export is
-/// disposed with before attempting to test the embed functionality.
-/// </summary>
-[Collection("CLI Tests")]
-public class CLITestEmbed : CLITestBase
-{
-    internal string TlbFilePath { get; }
-
-    internal string DependentTlbPath { get; }
-
-    public CLITestEmbed(CompileReleaseFixture compileFixture) : base(compileFixture)
-    {
-        var tlbFileName = $"{Path.GetFileNameWithoutExtension(TestAssemblyPath)}.tlb";
-
-        var result = Execute(DSComPath, "tlbexport", TestAssemblyPath);
-        result.ExitCode.Should().Be(0, $"because it should succeed. Error: ${result.StdErr}. Output: ${result.StdOut}");
-
-        result = Execute(DSComPath, "tlbexport", TestAssemblyDependencyPath);
-        result.ExitCode.Should().Be(0, $"because it should succeed. Error: ${result.StdErr}. Output: ${result.StdOut}");
-
-        TlbFilePath = Path.Combine(Environment.CurrentDirectory, tlbFileName);
-        DependentTlbPath = $"{Path.GetFileNameWithoutExtension(TestAssemblyDependencyPath)}.tlb";
-
-        File.Exists(TlbFilePath).Should().BeTrue($"File {TlbFilePath} should be available.");
-        File.Exists(DependentTlbPath).Should().BeTrue($"File {DependentTlbPath} should be available.");
-
-        // This is necessary to ensure the process from previous Execute for the export command has completely disposed before running tests.
-        GC.Collect();
-        GC.WaitForPendingFinalizers();
-    }
-
-    [StaFact]
-    public void TlbEmbedAssembly_ExitCodeIs0AndTlbIsEmbeddedAndValid()
-    {
-        var embedPath = GetEmbeddedPath(TestAssemblyPath);
-
-        var result = Execute(DSComPath, "tlbembed", TlbFilePath, embedPath);
-        result.ExitCode.Should().Be(0, $"because embedding should succeed. Output: {result.StdErr} ");
-
-        OleAut32.LoadTypeLibEx(embedPath, REGKIND.NONE, out var embeddedTypeLib);
-        OleAut32.LoadTypeLibEx(TlbFilePath, REGKIND.NONE, out var sourceTypeLib);
-
-        embeddedTypeLib.GetDocumentation(-1, out var embeddedTypeLibName, out _, out _, out _);
-        sourceTypeLib.GetDocumentation(-1, out var sourceTypeLibName, out _, out _, out _);
-
-        Assert.Equal(sourceTypeLibName, embeddedTypeLibName);
-    }
-
-    [StaFact]
-    public void TlbEmbedAssemblyWithArbitraryIndex_ExitCodeIs0AndTlbIsEmbeddedAndValid()
-    {
-        var embedPath = GetEmbeddedPath(TestAssemblyPath);
-        var result = Execute(DSComPath, "tlbembed", TlbFilePath, embedPath, "--index 2");
-        result.ExitCode.Should().Be(0);
-
-        OleAut32.LoadTypeLibEx(embedPath + "\\2", REGKIND.NONE, out var embeddedTypeLib);
-        OleAut32.LoadTypeLibEx(TlbFilePath, REGKIND.NONE, out var sourceTypeLib);
-
-        embeddedTypeLib.GetDocumentation(-1, out var embeddedTypeLibName, out _, out _, out _);
-        sourceTypeLib.GetDocumentation(-1, out var sourceTypeLibName, out _, out _, out _);
-
-        Assert.Equal(sourceTypeLibName, embeddedTypeLibName);
-    }
-
-    [StaFact]
-    public void TlbEmbedAssemblyWithArbitraryTlbAndArbitraryIndex_ExitCodeIs0AndTlbIsEmbeddedAndValid()
-    {
-        var embedPath = GetEmbeddedPath(TestAssemblyPath);
-        var result = Execute(DSComPath, "tlbembed", TlbFilePath, embedPath, "--index 3");
-        result.ExitCode.Should().Be(0);
-
-        OleAut32.LoadTypeLibEx(embedPath + "\\3", REGKIND.NONE, out var embeddedTypeLib);
-        OleAut32.LoadTypeLibEx(TlbFilePath, REGKIND.NONE, out var sourceTypeLib);
-
-        embeddedTypeLib.GetDocumentation(-1, out var embeddedTypeLibName, out _, out _, out _);
-        sourceTypeLib.GetDocumentation(-1, out var sourceTypeLibName, out _, out _, out _);
-
-        Assert.Equal(sourceTypeLibName, embeddedTypeLibName);
-    }
-
-    [StaFact]
-    public void TlbEmbedAssemblyWithMultipleTypeLibraries_ExitCodeAre0AndTlbsAreEmbeddedAndValid()
-    {
-        var embedPath = GetEmbeddedPath(TestAssemblyPath);
-        var result = Execute(DSComPath, "tlbembed", TlbFilePath, embedPath);
-        result.ExitCode.Should().Be(0);
-
-        result = Execute(DSComPath, "tlbembed", DependentTlbPath, TestAssemblyPath, "--index 2");
-        result.ExitCode.Should().Be(0);
-
-        OleAut32.LoadTypeLibEx(embedPath, REGKIND.NONE, out var embeddedTypeLib1);
-        OleAut32.LoadTypeLibEx(TlbFilePath, REGKIND.NONE, out var sourceTypeLib1);
-
-        embeddedTypeLib1.GetDocumentation(-1, out var embeddedTypeLibName1, out _, out _, out _);
-        sourceTypeLib1.GetDocumentation(-1, out var sourceTypeLibName1, out _, out _, out _);
-
-        Assert.Equal(sourceTypeLibName1, embeddedTypeLibName1);
-
-        OleAut32.LoadTypeLibEx(TestAssemblyPath + "\\2", REGKIND.NONE, out var embeddedTypeLib2);
-        OleAut32.LoadTypeLibEx(DependentTlbPath, REGKIND.NONE, out var sourceTypeLib2);
-
-        embeddedTypeLib2.GetDocumentation(-1, out var embeddedTypeLibName2, out _, out _, out _);
-        sourceTypeLib2.GetDocumentation(-1, out var sourceTypeLibName2, out _, out _, out _);
-
-        Assert.Equal(sourceTypeLibName2, embeddedTypeLibName2);
     }
 }
