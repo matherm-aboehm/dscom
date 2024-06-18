f30c63c773d7912a90038c032c96e74af3425d3b
Andreas Böhm <60391995+matherm-aboehm@users.noreply.github.com>
2024-06-18 14:43:43 +0200

    qa: Add more diagnostics to acceptance tests
    * Upload binlogs as artifacts if acceptance tests fail
    * Use correct MSBuild property to select TargetFramework for each build
      test (TargetPlatform property doesn't even exist for SDK-style projects)

--- examples/outproc/scripts/msbuild-acceptance-test.cmd
+++ examples/outproc/scripts/msbuild-acceptance-test.cmd
@@ -1,10 +1,17 @@
-@ECHO off
+@ECHO on
 
 SET root=%~dp0\..\..\..\
+SET logs=%~dp0\..\logs
+IF NOT EXIST %logs% MKDIR %logs%
 
 PUSHD %~dp0\..
 
-dotnet build
+dotnet build-server shutdown
+
+dotnet build -nodeReuse:False -bl:%logs%\build-runtime.binlog
+SET ERRUNTIME=%ERRORLEVEL%
+
+dotnet build-server shutdown
 
 POPD
 
@@ -12,9 +19,13 @@ SetLocal EnableDelayedExpansion
 
 SET EXITCODE=0
 
-IF NOT "%ERRORLEVEL%" == "0" (
+IF NOT "%ERRUNTIME%" == "0" (
   SET EXITCODE=1
-  ECHO "acceptance test failed."
+  ECHO "::warning::acceptance test failed."
+)
+
+IF "%EXITCODE%" == "0" (
+  ECHO "Acceptance test completed successfully."
 )
 
 EXIT /B %EXITCODE%
