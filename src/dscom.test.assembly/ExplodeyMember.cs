9d9bdf7440ad1b30f178a313252e5e573482c673
Andreas Böhm <60391995+matherm-aboehm@users.noreply.github.com>
2026-01-21 12:17:00 +0100

    test: Restore some files deleted in ac4946def9f776b32d9959063370dc504228afb1

---
+++ src/dscom.test.assembly/ExplodeyMember.cs
@@ -0,0 +1,9 @@
+using System.Windows.Controls;
+
+namespace dSPACE.Runtime.InteropServices.Test;
+
+public class ExplodeyMember
+{
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("Performance", "CA1822:Mark members as static", Justification = "Example")]
+    public UserControl Control => null!;
+}
