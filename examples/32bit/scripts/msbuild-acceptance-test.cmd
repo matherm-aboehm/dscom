3a858b8a37fb2f57f9e41e8ccf1651ea61a5e826
Andreas Böhm <60391995+matherm-aboehm@users.noreply.github.com>
2026-02-10 14:39:52 +0100

    Revert "build!: Removed dotnet 6 (#356)" partially
    This partially reverts commit a31c1bef275b407bbb1411928150becd4c93c042.
    Parts that build and release for .NET 8.0 by default are included, while
    other parts that remove .NET 6.0 support are reverted.

--- examples/32bit/scripts/msbuild-acceptance-test.cmd
+++ examples/32bit/scripts/msbuild-acceptance-test.cmd
@@ -39,7 +39,17 @@ dotnet build-server shutdown
 
 dotnet msbuild -nodeReuse:False -t:Build -p:Configuration=Release -p:Platform=x64 -p:TargetPlatform=net48 -p:PerformAcceptanceTest=Runtime -bl:%~dp0\net48x64.binlog
 
-SET ERRUNTIMEX64_net80=%ERRORLEVEL%
+SET ERRUNTIMEX64_NET48=%ERRORLEVEL%
+
+dotnet build-server shutdown
+
+dotnet msbuild -nodeReuse:False -t:Restore -p:Configuration=Release -p:Platform=x64 -p:TargetPlatform=net6.0-windows -p:PerformAcceptanceTest=Runtime
+
+dotnet build-server shutdown
+
+dotnet msbuild -nodeReuse:False -t:Build -p:Configuration=Release -p:Platform=x64 -p:TargetPlatform=net6.0-windows -p:PerformAcceptanceTest=Runtime -bl:%~dp0\net60x64.binlog
+
+SET ERRUNTIMEX64_NET60=%ERRORLEVEL%
 
 dotnet build-server shutdown
 
@@ -59,6 +69,15 @@ dotnet build-server shutdown
 
 dotnet msbuild -nodeReuse:False -t:Build -p:Configuration=Release -p:Platform=x86 -p:TargetPlatform=net48 -p:PerformAcceptanceTest=Runtime -bl:%~dp0\net48x86.binlog
 SET ERRUNTIMEX86_NET48=%ERRORLEVEL%
+
+dotnet build-server shutdown
+
+dotnet msbuild -nodeReuse:False -t:Restore -p:Configuration=Release -p:Platform=x86 -p:TargetPlatform=net6.0-windows -p:PerformAcceptanceTest=Runtime
+
+dotnet build-server shutdown
+
+dotnet msbuild -nodeReuse:False -t:Build -p:Configuration=Release -p:Platform=x86 -p:TargetPlatform=net6.0-windows -p:PerformAcceptanceTest=Runtime -bl:%~dp0\net60x86.binlog
+SET ERRUNTIMEX86_NET60=%ERRORLEVEL%
 
 dotnet build-server shutdown
 
@@ -84,6 +103,11 @@ IF NOT "%ERRUNTIMEX64_NET48%" == "0" (
   ECHO "::warning::Runtime specific acceptance test for platform x64 using .NET FullFramework 4.8 failed."
 )
 
+IF NOT "%ERRUNTIMEX64_NET60%" == "0" (
+  SET EXITCODE=1
+  ECHO "::warning::Runtime specific acceptance test for platform x64 using .NET 6.0 failed."
+)
+
 IF NOT "%ERRUNTIMEX64_NET80%" == "0" (
   SET EXITCODE=1
   ECHO "::warning::Runtime specific acceptance test for platform x64 using .NET 8.0 failed."
@@ -97,6 +121,21 @@ IF NOT "%ERRUNTIMEX86_NET48%" == "0" (
 IF NOT "%ERRUNTIMEX86_NET80%" == "0" (
   ::SET EXITCODE=1
   ECHO "::warning::Runtime specific acceptance test for platform x86 using .NET 8.0 failed."
+)
+
+IF NOT "%ERRUNTIMEX86_NET60%" == "0" (
+  ::SET EXITCODE=1
+  ECHO "::warning::Runtime specific acceptance test for platform x64 using .NET 6.0 failed."
+)
+
+IF NOT EXIST %~dp0\..\comtestdotnet\bin\x64\Release\net6.0\comtestdotnet.tlb (
+  SET EXITCODE=1
+  ECHO "::warning::Could not find exported TLB file for .NET 6 (x64)"
+)
+
+IF NOT EXIST %~dp0\..\comtestdotnet\bin\x86\Release\net6.0\comtestdotnet.tlb (
+  ::SET EXITCODE=1
+  ECHO "::warning::Could not find exported TLB file for .NET 6 (x86)"
 )
 
 IF NOT EXIST %~dp0\..\comtestdotnet\bin\x64\Release\net48\comtestdotnet.tlb (
